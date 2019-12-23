7bba9b38709c208b429a383d543eb4b7186f5426
yanina <yaninaolmedo27@gmail.com>
2019-12-22 23:30:50 -0300

    IN1025-1 - We have the carousell

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_files"),
@@ -135,8 +136,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(
     os.path.dirname(BASE_DIR), 'inmobiliaria', "static_cdn", "static_root"
 )
-
-MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(
     os.path.dirname(BASE_DIR), 'inmobiliaria', 'static_cdn', 'media_root'
--- search/views.py
+++ search/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from search.models import Estate, Image
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    latest = Estate.objects.all().order_by('-created')[:3]
+    images = Image.objects.filter(estate__in=latest).distinct('estate')
+
+    context = {
+        'images': images
+    }
+    return render(request, 'index.html', context)
