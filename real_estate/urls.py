61f0258e2abb5a51996fd29f4fe7c5c7dcc462f4
Martín Manganiello <francomartinmanganiello@gmail.com>
2019-12-25 17:40:45 -0300

    Lib exclude from gitignore

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -24,10 +24,9 @@ urlpatterns = [
     path('', index),
 ]
 
-if settings.DEBUG:
-    urlpatterns = urlpatterns + static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
-    urlpatterns = urlpatterns + static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+urlpatterns = urlpatterns + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+)
+urlpatterns = urlpatterns + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
