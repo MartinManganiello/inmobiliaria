93f5db24a35f80ba4adedaf2f06e396b20c47127
Martín Manganiello <francomartinmanganiello@gmail.com>
2019-12-25 16:53:46 -0300

    Debug true

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k+_y0&-+l^@7w+y3%6#c4wav1bvd7k9p(z%m5m-c00*5ssu7$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -79,6 +79,12 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
 
 if DEBUG:
     DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'real_estate',
@@ -87,12 +93,6 @@ if DEBUG:
             'HOST': 'localhost',
             'PORT': '5432',
         }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=config('DATABASE_URL')
-        )
     }
 
 
