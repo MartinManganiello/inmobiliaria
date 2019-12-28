c5ecbfbaef406337ba1ee74d924b5ddaa318d70f
Martín Manganiello <francomartinmanganiello@gmail.com>
2019-12-28 17:48:18 -0300

    Add another app

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'search'
+    'search',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
