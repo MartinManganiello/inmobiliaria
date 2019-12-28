9165ddbab605e22d6ff8f000d3679684bf94a730
Martín Manganiello <francomartinmanganiello@gmail.com>
2019-12-28 16:46:42 -0300

    Did some changes over the settings

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -17,18 +17,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from search.views import index, about, properties, contact
+from search import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('nosotros/', about,  name='about'),
-    path('propiedades/', properties,  name='properties'),
-    path('contacto/', contact,  name='contact'),
+    path('', views.index, name='index'),
+    path('nosotros/', views.about,  name='about'),
+    path('propiedades/', views.properties,  name='properties'),
+    path('contacto/', views.contact,  name='contact'),
 ]
-urlpatterns += static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT
-)
-urlpatterns += static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
