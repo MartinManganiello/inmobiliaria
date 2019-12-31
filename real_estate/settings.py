a370fcd047a1b27787a387dad4c7bc4e377043b8
Martín Manganiello <francomartinmanganiello@gmail.com>
2019-12-31 11:46:40 -0300

    WIP

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -142,9 +142,9 @@ TEMPLATE_DIRS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static-root")
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles", "media-root")
 
 
 STATICFILES_STORAGE = 'real_estate.storage.WhiteNoiseStaticFilesStorage'
