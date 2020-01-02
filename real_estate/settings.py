c350b5ba2da948f62906fc05087c7b71f5198111
Martín Manganiello <francomartinmanganiello@gmail.com>
2020-01-02 14:03:11 -0300

    Remove whitenoise references

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = '{PROJECT_NAME}.urls'.format(PROJECT_NAME=PROJECT_NAME)
